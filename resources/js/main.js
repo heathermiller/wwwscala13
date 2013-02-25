7482d78dc019ba9c461e8935c676e8bf1e3ac4f9
heathermiller <heather.miller@epfl.ch>
2013-02-25 14:37:56 +0100

    Fixing the js so as to be able to run it remotely on the server

--- resources/js/main.js
+++ resources/js/main.js
@@ -4,7 +4,7 @@
 $(window).bind("load", function() {
   var path = window.location.pathname;
   var page = path.substring(path.lastIndexOf('/') + 1);
-  var is_root = location.pathname == "/";
+  var is_root = location.pathname == "/~hmiller/scala2013/";
 
   if (is_root || page == "index.html") {
     $('.white-fullwidth-bar').sticky({topSpacing:0, className:'unhide'});
